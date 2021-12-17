158536fcf49714c07e80330d5213268d0c844234
bananamonkey4655 <vanillaqicecream@gmail.com>
2021-12-17 16:29:24 +0800

    Added loops to repeat game in browser

--- script.js
+++ script.js
@@ -9,21 +9,57 @@ function computerPlay() {
     }
 }
 
-function playerSelection() {
+function getPlayerSelection() {
     const input = prompt('Rock, paper or scissors?');
     const choice = input.toUpperCase();
 
     if (choice === 'ROCK' || choice === 'PAPER' || choice === 'SCISSORS') {
         return choice;
     } else {
-        return playerSelection();
+        return getPlayerSelection();
     }
 }
 
-function computerSelection() {
+function getComputerSelection() {
     return computerPlay().toUpperCase();
 }
 
+function getWinner(playerSelection, computerSelection) {
+    const winStatement = `You win! ${playerSelection} beats ${computerSelection}`;
+    const loseStatement = `You lose! ${computerSelection} beats ${playerSelection}`;
+    if (playerSelection === 'ROCK') {
+        if (computerSelection === 'SCISSORS') {
+            return winStatement;
+        } else {
+            return loseStatement;
+        }
+    } else if (playerSelection === 'PAPER') {
+        if (computerSelection === 'ROCK') {
+            return winStatement;
+        } else {
+            return loseStatement;
+        }
+    } else {
+        if (computerSelection === 'PAPER') {
+            return winStatement;
+        } else {
+            return loseStatement;
+        }
+    }
+}
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'It\'s a draw! Try again.';
+    } else {
+        return getWinner(playerSelection, computerSelection);
+    }
+}
 
-console.log(computerSelection());
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(getPlayerSelection(), getComputerSelection()));
+    }
+}
+
+game();
