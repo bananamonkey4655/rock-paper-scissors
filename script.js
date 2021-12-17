922926a5b3cc56971fd8fb510c874c3bf4e29ce8
bananamonkey4655 <vanillaqicecream@gmail.com>
2021-12-17 16:08:48 +0800

    Added component functions to get input from user and computer

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function computerPlay() {
-    return Math.random();
+    const num = Math.floor(3 * Math.random());
+    if (num === 0) {
+        return 'Rock';
+    } else if (num === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
 }
 
-computerPlay();
+function playerSelection() {
+    const input = prompt('Rock, paper or scissors?');
+    const choice = input.toUpperCase();
+
+    if (choice === 'ROCK' || choice === 'PAPER' || choice === 'SCISSORS') {
+        return choice;
+    } else {
+        return playerSelection();
+    }
+}
+
+function computerSelection() {
+    return computerPlay().toUpperCase();
+}
+
+
+
+console.log(computerSelection());
